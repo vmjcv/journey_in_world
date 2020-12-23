d2315e6b59673be0cde4b159f29501c511340338
vmjcv666 <vmjcv666@gmail.com>
2020-12-24 03:26:08 +0800

    add

---
+++ game/test/test.gd
@@ -0,0 +1,11 @@
+extends CSGBox
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"卡片ui/MeshInstance".get_surface_material(0).set_shader_param("stencil_color",$"Viewport/Control/ViewportContainer/Viewport/主体画面".color)
