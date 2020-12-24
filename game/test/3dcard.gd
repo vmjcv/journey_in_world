bde35b7d1cd75df3b84bc59d3416f26f1ed9b430
vmjcv666 <vmjcv666@gmail.com>
2020-12-25 01:43:19 +0800

    3d 卡牌旋转速度改变

--- game/test/3dcard.gd
+++ game/test/3dcard.gd
@@ -2,5 +2,5 @@ extends Spatial
 
 func _process(delta: float) -> void:
 #	rotate_y(deg2rad(10) * delta)
-	rotate_z(deg2rad(20) * delta)
+	rotate_z(deg2rad(40) * delta)
 #	rotate_x(deg2rad(10) * delta)
