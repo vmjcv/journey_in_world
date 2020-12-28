decc1b311aa8b2e6f78ac4d48c92427a6da83c63
vmjcv666 <vmjcv666@gmail.com>
2020-12-29 01:20:57 +0800

    改变节点的层次结构

---
+++ game/test/card.gd
@@ -0,0 +1,7 @@
+extends Spatial
+
+func _process(delta: float) -> void:
+#	rotate_y(deg2rad(10) * delta)
+	rotate_z(deg2rad(40) * delta)
+#	rotate_x(deg2rad(10) * delta)
+	pass
---
+++ game/test/set_camera.gd
@@ -0,0 +1,26 @@
+extends Spatial
+
+var all_nodes = []
+
+export var camera_layer = 8
+
+func _ready():
+	get_all_nodes(self)
+	change_child_camera_layer()
+
+func get_all_nodes(node):
+	for N in node.get_children():
+		if N.get_child_count() > 0:
+			get_all_nodes(N)
+		else:
+			all_nodes.append(N)
+
+func change_child_camera_layer():
+	for n in all_nodes:
+		if n is VisualInstance:
+			n.layers = camera_layer
+			print("VisualInstance",camera_layer)
+		if n is Camera:
+			n.cull_mask = camera_layer
+			print("Camera",camera_layer)
+	
