578a9f14ca23d2f0396869c9877ada6793bb2de3
majinchuan <vmjcv666@gmail.com>
2020-12-29 11:24:29 +0800

    暂时对所有mesh手动设定material override，排除不显示的问题，初步定位为center_scene为一个空的

--- game/test/card_face.shader
+++ game/test/card_face.shader
@@ -9,19 +9,20 @@ uniform vec4 stencil_color : hint_color;
 void fragment() {
 	vec4 stencil_test = texture(stencil, SCREEN_UV);
 	vec4 color_test = stencil_color;
-	
+
 	float stencil_id = stencil_test.r * 10.0 + stencil_test.g + 100.0 * stencil_test.b + 1000.0;
 	float mask_id = stencil_color.r * 10.0 + stencil_color.g + 100.0 * stencil_color.b + 1000.0;
-	
+
 	float mask_result = abs(stencil_id - mask_id);
-	
+
 	if(mask_result == 0.0){
-		ALPHA = 0.5;
-		ALBEDO = vec3(0.3,0.4,1.0);
+		ALPHA = 0.0; // 中间的部分透明
 	}
 	else{
 		vec4 end_color = texture(center_scene, SCREEN_UV);
 		ALPHA = end_color.a;
 		ALBEDO = end_color.rgb;
+		ALPHA = 0.5;
+		ALBEDO = vec3(1.0,0.5,0.3);
 	}
 }
--- game/test/change_next_pass.gd
+++ game/test/change_next_pass.gd
@@ -19,17 +19,17 @@ func get_all_nodes(node):
 func change_child_camera_layer():
 	for n in all_nodes:
 		if n is VisualInstance:
-			n.material_override = ShaderMaterial.new()
-			n.material_override.resource_local_to_scene = true
-			n.material_override.shader = preload("res://test/card_face.shader")
-			var stencil_viewport = ViewportTexture.new()
-			stencil_viewport.viewport_path = "卡牌/卡面/显示视口"
-			n.material_override.set_shader_param("stencil",stencil_viewport)
-			
+			# n.material_override = ShaderMaterial.new()
+			# n.material_override.resource_local_to_scene = true
+			# n.material_override.shader = preload("res://test/card_face.shader")
+			# var stencil_viewport = ViewportTexture.new()
+			# stencil_viewport.viewport_path = "卡牌/卡面/显示视口"
+			# n.material_override.set_shader_param("stencil",stencil_viewport)
 
-			var obj_viewport = ViewportTexture.new()
-			obj_viewport.viewport_path = "卡牌/卡面/Viewport"
-			n.material_override.set_shader_param("center_scene",obj_viewport)
-			
-			n.material_override.set_shader_param("stencil_color",Color.white)
+
+			# var obj_viewport = ViewportTexture.new()
+			# obj_viewport.viewport_path = "卡牌/卡面/Viewport"
+			# n.material_override.set_shader_param("center_scene",obj_viewport)
+
+			# n.material_override.set_shader_param("stencil_color",Color.white)
 			pass
