420aac316cde62f0d5e72619801e96b4d0551525
vmjcv666 <vmjcv666@gmail.com>
2021-09-09 04:11:32 +0800

    修改起始状态

---
+++ game/addons/localization_editor/LocalizationManagerKeys.gd
@@ -0,0 +1,20 @@
+# Keys for LocalizationManger to use in source code: MIT License
+# @author Vladimir Petrenko
+tool
+class_name LocalizationManagerKeys
+
+const KEY_PLAY = "KEY_PLAY"
+const KEY_SETTINGS = "KEY_SETTINGS"
+const KEY_LANGUAGE = "KEY_LANGUAGE"
+const KEY_BACK = "KEY_BACK"
+const KEY_QUIT = "KEY_QUIT"
+const KEY_START = "KEY_START"
+
+const KEYS = [
+ "KEY_PLAY",
+ "KEY_SETTINGS",
+ "KEY_LANGUAGE",
+ "KEY_BACK",
+ "KEY_QUIT",
+ "KEY_START"
+]
